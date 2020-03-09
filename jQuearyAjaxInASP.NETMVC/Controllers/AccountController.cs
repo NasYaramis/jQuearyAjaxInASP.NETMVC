ba7e1b3471f00c817e500b6990324b7106b7f27d
Yaramis <Naseyb.Yaramis@ordina.be>
2020-03-09 11:10:41 +0100

    Add project files.

---
+++ jQuearyAjaxInASP.NETMVC/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using jQuearyAjaxInASP.NETMVC.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace jQuearyAjaxInASP.NETMVC.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [Route("api/User/Register")]
+        [HttpPost]
+        public IdentityResult Register(AccountModel model)
+        {
+            var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var manager = new UserManager<ApplicationUser>(userStore);
+            var user = new ApplicationUser() { UserName = model.UserName, Email = model.Email };
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            IdentityResult result = manager.Create(user, model.Password);
+            return result;
+        }
+    }
+}
---
+++ jQuearyAjaxInASP.NETMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jQuearyAjaxInASP.NETMVC.MvcApplication" Language="C#" %>
